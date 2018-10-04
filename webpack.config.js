4a20aaef6cd4842b71c48503eb00679a9a7305d5
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2018-10-04 18:03:04 +0200

    add babel to webpack config, throws an error with preset.

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,34 @@ const path = require('path');
 module.exports = {
   entry: './js/main.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets:
+           [[
+             "@babel/preset-env",
+             {
+               "targets":
+              {
+                "chrome": 52,
+                "browsers": ["Firefox 49", "Edge 12","Chrome 53", "Opera 41", "Safari 10"],
+              },
+               "useBuiltIns": "usage",
+             },
+           ]],
+
+        },
+      },
+    ],
+  },
+  stats: {
+    colors: true,
+  },
+  devtool: 'source-map',
 };
